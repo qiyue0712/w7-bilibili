1cdde0c07f12bed56862773f1adedf3a1c062f1a
qiyue0712 <13641913653@163.com>
2025-12-28 03:38:46 +0800

    项目初始化构建

---
+++ src/views/Layout.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div>测试页面</div>
+  <div>
+    <el-button type="primary" @click="">
+      按钮
+    </el-button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
